3fd0ec9617d97a21fa7dcd6b270a5c2185480932
Sultonkulov99 <abdukhoshim99@gmail.com>
2025-02-15 17:35:40 +0500

    auth register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -10,7 +9,6 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true
     }),
     MongooseModule.forRoot("mongodb+srv://Abdukoshim:0bEqB4ywyJll4aAE@cluster0.pkuhx.mongodb.net/demo?retryWrites=true&w=majority&appName=Cluster0"),
-    UsersModule, 
     AuthModule
   ]
 })
